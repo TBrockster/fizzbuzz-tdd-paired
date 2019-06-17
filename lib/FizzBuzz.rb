f3a1fb647f81f3ede970441b64b40ed82f414622
EC2 Default User <ec2-user@ip-172-31-42-207.us-east-2.compute.internal>
2019-06-17 20:17:07 +0000

    final commit

--- lib/FizzBuzz.rb
+++ lib/FizzBuzz.rb
@@ -1,4 +1,11 @@
-def FizzBuzz(number)
-  'Fizz'
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+      'FizzBuzz'
+  elsif number % 3 == 0
+      'Fizz'
+  elsif number % 5 == 0
+      'Buzz'
+  else 
+      number
+  end
 end
-
--- spec/FizzBuzz_spec.rb
+++ spec/FizzBuzz_spec.rb
@@ -1,6 +1,28 @@
 require_relative '../lib/FizzBuzz'
-describe 'FizzBuzz' do
+
+describe 'fizzbuzz' do
     it 'returns "Fizz" when passed 3' do
-        expect(FizzBuzz(3)).to eq 'Fizz'
+        expect(fizzbuzz(3)).to eq 'Fizz'
+    end
+    it 'returns "fizzbuzz" when passed 15' do
+        expect(fizzbuzz(15)).to eq 'FizzBuzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns 1 when passed 1' do
+        expect(fizzbuzz(1)).to eq 1
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns 2 when passed 2' do
+        expect(fizzbuzz(2)).to eq 2
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "Buzz" when passed 5' do
+        expect(fizzbuzz(5)).to eq 'Buzz'
     end
 end
